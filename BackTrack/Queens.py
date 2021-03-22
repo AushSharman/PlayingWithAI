56edc2a56fa47026a5b294bff58b726733e54ac0
Ayush <ayushireland@gmail.com>
2021-03-22 17:59:47 +0000

    Validation of moves and addition of recursive element

--- BackTrack/Queens.py
+++ BackTrack/Queens.py
@@ -8,15 +8,16 @@ def printBoard(dimension: int) -> list:
     return dim  
 
 def isValidMove(board: list, row: int, col: int) -> bool:
+    #print(row ," is row and col us ", col)
     #check up meaning the col is same but row reduces
     for i in range(row,-1,-1):
         if board[i][col] == 1:
             return False
-    #check the left upper diagonal
+    #check the left upper diagonal meaning both row and col decrease
     for r,c in zip(range(row,-1,-1),range(col,-1,-1)):
         if board[r][c] == 1:
             return False
-    #check the right upper diagonal
+    #check the right upper diagonal meaning that row decreases and col increases
     for r,c in zip(range(row,-1,-1),range(col+1)):
         if board[r][c] ==1 :
             return False
@@ -28,9 +29,11 @@ def solve(board, row = 0, col = 0):
         return True
     else:
         for position in range(dimension):
-            if isValidMove(board,row,col):
-                value = solve(board,row+1,col)
-                return value
+            if isValidMove(board,row,position):
+                board[row][position] = 1
+                print(board)
+                value = solve(board,row+1,position+1)
+            board[row][position] = 0
         return False
 
 if __name__ == "__main__":
@@ -39,3 +42,5 @@ if __name__ == "__main__":
         print(board)
     else:
         print("Cannot be solved")
+
+#TODO: Adjust the alogorithim as output is incorrect 
